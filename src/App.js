4896e5ebc1dd37d5e3521dbd2118d21105e30faf
Jetty <kmclo@naver.com>
2020-01-26 23:28:10 +0900

    #3 Portals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
 
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
+
+const Message = () => "Just touched it!";
 class ReturnTypes extends Component {
   render() {
     return "hello";
@@ -11,6 +19,7 @@ class App extends Component {
     return (
       <Fragment>
         <ReturnTypes />
+        <Portals />
       </Fragment>
     );
   }
